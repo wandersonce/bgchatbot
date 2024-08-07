6224c099ea2c773d55df08c992aad28965d6df63
Wanderson Castro <wandersonce1@hotmail.com>
2024-08-07 11:20:21 -0700

    Add: Apollo Route

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,13 +1,49 @@
-import { NextRequest } from 'next/server';
+import { serverClient } from '@/lib/server/serverClient';
+import { gql } from '@apollo/client';
+import { NextRequest, NextResponse } from 'next/server';
+
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+};
 
 export async function POST(request: NextRequest) {
   const { query, variables } = await request.json();
 
   try {
+    let result;
     if (query.trim().startsWith('mutation')) {
       //handle mutations
+      result = await serverClient.mutate({
+        mutation: gql`
+          ${query}
+        `,
+        variables,
+      });
     } else {
       //handle queries
+      result = await serverClient.query({
+        query: gql`
+          ${query}
+        `,
+        variables,
+      });
     }
-  } catch (error) {}
+
+    const data = result.data;
+    console.log('DATA >>> ', data);
+
+    return NextResponse.json(
+      {
+        data,
+      },
+      {
+        headers: corsHeaders,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(error, { status: 500 });
+  }
 }
