c55296ce0df5e324970343ab7a4cdbb03fee16f5
Wanderson Castro <wandersonce1@hotmail.com>
2024-08-07 11:13:56 -0700

    ADD: apollo sever client

---
+++ src/app/api/graphql/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest } from 'next/server';
+
+export async function POST(request: NextRequest) {
+  const { query, variables } = await request.json();
+
+  try {
+    if (query.trim().startsWith('mutation')) {
+      //handle mutations
+    } else {
+      //handle queries
+    }
+  } catch (error) {}
+}
