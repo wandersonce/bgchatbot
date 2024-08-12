cd682e7a20c7fe103597485aa76aa3760141a3e8
Wanderson Castro <wandersonce1@hotmail.com>
2024-08-12 12:54:52 -0700

    Add: Toast and Copy to Clipboard chatbot link

--- src/app/(admin)/edit-chatbot/[id]/page.tsx
+++ src/app/(admin)/edit-chatbot/[id]/page.tsx
@@ -1,5 +1,51 @@
-function EditChatbot() {
-  return <div>page</div>;
+'use client';
+
+import { Input } from '@/components/ui/input';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { BASE_URL } from '../../../../../graphql/apolloClient';
+import { Button } from '@/components/ui/button';
+import { Copy } from 'lucide-react';
+import { typeFromAST } from 'graphql';
+import { toast } from 'sonner';
+
+function EditChatbot({ params: { id } }: { params: { id: string } }) {
+  const [url, setUrl] = useState<string>('');
+
+  useEffect(() => {
+    const url = `${BASE_URL}/chatbot/${id}`;
+
+    setUrl(url);
+  }, [id]);
+
+  return (
+    <div className="px-0 md:p-10">
+      <div className="md:sticky md:top-0 z-50 sm:max-w-sm ml-auto space-y-2 md:border p-5 rounded-b-lg md:rounded-lg bg-[#2991EE]">
+        <h2 className="text-white text-sm font-bold">Link to Chat</h2>
+        <p className="text-sm text-white">
+          Share this link with your audience to start conversations with you
+          chatbot!
+        </p>
+        <div className="flex items-center space-x-2">
+          <Link href={url} className="w-full cursor-pointer hover:opacity-50">
+            <Input value={url} readOnly className="cursor-pointer" />
+          </Link>
+          <Button
+            type="submit"
+            size="sm"
+            className="px-3"
+            onClick={() => {
+              navigator.clipboard.writeText(url);
+              toast.success('Copied to clipboard');
+            }}
+          >
+            <span className="sr-only">Copy</span>
+            <Copy className="h-4 w-4" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default EditChatbot;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 import ApolloProviderWrapper from '@/components/ApolloProvider';
+import { Toaster } from '@/components/ui/sonner';
 
 export const metadata: Metadata = {
   title: 'Bam Tech & Setup',
@@ -20,6 +21,7 @@ export default function RootLayout({
           <body className="min-h-screen flex">
             {children}
             {/* Toaster */}
+            <Toaster position="bottom-center" />
           </body>
         </html>
       </ClerkProvider>
