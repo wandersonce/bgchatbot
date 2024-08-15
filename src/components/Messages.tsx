da2d4e31a949b0057eea3c5380d7f5c4772b1d49
Wanderson Castro <wandersonce1@hotmail.com>
2024-08-15 11:39:05 -0700

    UPD: Messages Component

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,6 +1,9 @@
 'use client';
 
+import { usePathname } from 'next/navigation';
 import { Message } from '../../types/types';
+import Avatar from './Avatar';
+import { UserCircle } from 'lucide-react';
 
 function Messages({
   messages,
@@ -9,7 +12,49 @@ function Messages({
   messages: Message[];
   chatbotName: string;
 }) {
-  return <div>Messages</div>;
+  const path = usePathname();
+  const isReviewPage = path.includes('review-sessions');
+  return (
+    <div>
+      {messages.map((message) => {
+        const isSender = message.sender !== 'user';
+
+        return (
+          <div
+            key={message.id}
+            className={`chat ${isSender ? 'chat-start' : 'chat-end'} relative`}
+          >
+            {isReviewPage && (
+              <p className="absolute -bottom-5 text-xs text-gray-300">
+                sent {new Date(message.created_at).toLocaleString()}
+              </p>
+            )}
+
+            <div className={`chat-image avatar w-10 ${!isSender && '-mr-4'}`}>
+              {isSender ? (
+                <Avatar
+                  seed={chatbotName}
+                  className="h-12 w-12 bg-white rounded-full border-2 border-[#2991EE]"
+                />
+              ) : (
+                <UserCircle className="text-[#2991EE]" />
+              )}
+            </div>
+
+            <p
+              className={`chat-bubble text-white ${
+                isSender
+                  ? 'chat-bubble-primary bg-[#4D7DFB]'
+                  : 'chat-bubble-secondary bg-gray-200 text-gray-700'
+              }`}
+            >
+              Hello
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Messages;
