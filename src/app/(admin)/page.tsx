53a97db93570e83eb156818c92f2f1b58cb59746
Wanderson Castro <wandersonce1@hotmail.com>
2024-08-15 12:40:47 -0700

    ADD: Home admin page

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -1,7 +1,22 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Bam Setup Chatbot</h1>
+    <main className="p-10 bg-white m-10 rounded-md w-full">
+      <h1 className="text-4xl font-light">
+        Welcome to{' '}
+        <span className="text-[#64B5F5] font-semibold">Bam Chatbot</span>
+      </h1>
+      <h2 className="mt-2 mb-10">
+        Your AI agent that will help you manage your audience questions.
+      </h2>
+
+      <Link href="/create-chatbot">
+        <Button className="bg-[#64B5F5]">
+          Lets get started by creating your first chatbot
+        </Button>
+      </Link>
     </main>
   );
 }
