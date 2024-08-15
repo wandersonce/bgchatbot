a63c9c2f91a277aabada7a2f10a53dc5c8583905
Wanderson Castro <wandersonce1@hotmail.com>
2024-08-15 10:54:54 -0700

    ADD: CHATBOTs sessions component

--- src/components/ChatbotSessions.tsx
+++ src/components/ChatbotSessions.tsx
@@ -1,7 +1,17 @@
 'use client';
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+import ReactTimeago from 'react-timeago';
+
 import { useEffect, useState } from 'react';
 import { Chatbot } from '../../types/types';
+import Avatar from './Avatar';
+import Link from 'next/link';
 
 function ChatbotSessions({ chatbots }: { chatbots: Chatbot[] }) {
   const [sortedChatbots, setSortedChatbots] = useState<Chatbot[]>([]);
@@ -9,13 +19,66 @@ function ChatbotSessions({ chatbots }: { chatbots: Chatbot[] }) {
   //This sort chatbots by quantity of sessions
   useEffect(() => {
     const sortedArray = [...chatbots].sort(
-      (a, b) => b.chat_sessions.length - b.chat_sessions.length
+      (a, b) => b.chat_sessions.length - a.chat_sessions.length
     );
 
     setSortedChatbots(sortedArray);
   }, [chatbots]);
 
-  return <div>ChatbotSessions</div>;
+  return (
+    <div>
+      <Accordion type="single" collapsible>
+        {sortedChatbots.map((chatbot) => {
+          const hasSessions = chatbot.chat_sessions.length > 0;
+
+          return (
+            <AccordionItem
+              key={chatbot.id}
+              value={`item-${chatbot.id}`}
+              className="px-10 py-5"
+            >
+              {hasSessions ? (
+                <>
+                  <AccordionTrigger>
+                    <div className="flex text-left items-center w-full">
+                      <Avatar seed={chatbot.name} className="h-10 w-10 mr-4" />
+                      <div className="flex flex-1 justify-between space-x-4">
+                        <p>{chatbot.name}</p>
+                        <p className="pr-4 font-bold text-right">
+                          {chatbot.chat_sessions.length} sessions
+                        </p>
+                      </div>
+                    </div>
+                  </AccordionTrigger>
+                  <AccordionContent className="space-y-5 p-5 bg-gray-100">
+                    {chatbot.chat_sessions.map((session) => (
+                      <Link
+                        href={`/review-sessions/${session.id}`}
+                        key={session.id}
+                        className="relative p-10 bg-[#2991EE] text-white rounded-md block"
+                      >
+                        <p className="text-lg font-bold">
+                          {session.guests?.name || 'Anonymous'}
+                        </p>
+                        <p className="text-sm font-light">
+                          {session.guests?.email || 'No email provided'}
+                        </p>
+                        <p className="absolute top-5 right-5 text-sm">
+                          <ReactTimeago date={new Date(session.created_at)} />
+                        </p>
+                      </Link>
+                    ))}
+                  </AccordionContent>
+                </>
+              ) : (
+                <p className="font-light">{chatbot.name} (No Sessions)</p>
+              )}
+            </AccordionItem>
+          );
+        })}
+      </Accordion>
+    </div>
+  );
 }
 
 export default ChatbotSessions;
