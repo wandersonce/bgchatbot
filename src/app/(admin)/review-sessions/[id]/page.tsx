a051690c7a240c42a64559f48be13670e4a3aed9
Wanderson Castro <wandersonce1@hotmail.com>
2024-08-15 11:21:52 -0700

    Add: Daisy UI and starting messages component

--- src/app/(admin)/review-sessions/[id]/page.tsx
+++ src/app/(admin)/review-sessions/[id]/page.tsx
@@ -4,6 +4,7 @@ import {
   GetChatSessionMessagesResponse,
   GetSessionMessagesVariables,
 } from '../../../../../types/types';
+import Messages from '@/components/Messages';
 
 export const dynamic = 'force-dynamic';
 
@@ -25,7 +26,24 @@ async function ReviewSession({ params: { id } }: { params: { id: string } }) {
     query: GET_CHAT_SESSION_MESSAGES,
     variables: { id: parseInt(id as string) },
   });
-  return <div>page</div>;
+  return (
+    <div className="flex-1 p-10 pb-24">
+      <h1 className="text-xl lg:text-3xl font-semibold">Session Review</h1>
+      <p className="font-light text-xs text-gray-400 mt-2">
+        Started at {new Date(created_at).toLocaleString()}
+      </p>
+      <h2 className="font-light mt-2">
+        Between {name} &{' '}
+        <span className="font-extrabold">
+          {guestName} ({email})
+        </span>
+      </h2>
+
+      <hr className="my-10" />
+
+      <Messages messages={messages} chatbotName={name} />
+    </div>
+  );
 }
 
 export default ReviewSession;
---
+++ src/components/Messages.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { Message } from '../../types/types';
+
+function Messages({
+  messages,
+  chatbotName,
+}: {
+  messages: Message[];
+  chatbotName: string;
+}) {
+  return <div>Messages</div>;
+}
+
+export default Messages;
